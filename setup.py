142d4f905d392547ee1eccbf69d9061f9befe0bb
NoguchiRyo <noguchi-ryo000@g.ecc.u-tokyo.ac.jp>
2025-07-08 07:14:04 +0900

    Fix typo in CI.yml and add setup.py to change metadata in whl

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, Distribution
+
+
+class BinaryDistribution(Distribution):
+    def has_ext_modules(self):
+        return True
+
+
+setup(
+    distclass=BinaryDistribution,
+)
