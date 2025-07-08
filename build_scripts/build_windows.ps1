9b7cc006cd7053a707514ebb81436325276a0b52
NoguchiRyo <noguchi-ryo000@g.ecc.u-tokyo.ac.jp>
2025-07-08 12:22:24 +0900

    bugfix in windows building

--- build_scripts/build_windows.ps1
+++ build_scripts/build_windows.ps1
@@ -2,6 +2,16 @@ $py = python -c "import sys, pathlib; print(pathlib.Path(sys.executable).resolve
 Write-Host "Using Python: $py"
 & $py build_scripts/generate_cmake_presets.py
 & $py --version
-cmake -B build --preset=default -DPython_EXECUTABLE=$py -DPython3_EXECUTABLE=$py -DPython_FIND_REGISTRY=NEVER -DPython3_FIND_REGISTRY=NEVER
+
+cmake -B build --preset=default `
+  -DPython_EXECUTABLE="$py" `
+  -DPython3_EXECUTABLE="$py" `
+  -DPython_ROOT_DIR="$(Split-Path $py)" `
+  -DPython_FIND_STRATEGY=LOCATION `
+  -DPython_FIND_REGISTRY=NEVER `
+  -DPython3_FIND_REGISTRY=NEVER `
+  -DPython_FIND_FRAMEWORK=NEVER `
+  -DPython3_FIND_FRAMEWORK=NEVER
+
 cmake --build build --config Release -j
 cmake --install build --config Release
